3ef8e8e97cc458fd81ba386b070d2cfc433b22e3
Raul Armas <you@example.com>
2023-05-31 13:24:06 -0500

    updates

---
+++ SolutionWCFRest/ServicioWCFRest/IGraphQLService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioWCFRest
+{
+    [ServiceContract]
+    public interface IGraphQLService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        Message ExecuteGraphQL(Message request);
+    }
+
+}
--- SolutionWCFRest/ServicioWCFRest/ServicioEmpleado.svc.cs
+++ SolutionWCFRest/ServicioWCFRest/ServicioEmpleado.svc.cs
@@ -1,16 +1,31 @@
-﻿using ServicioWCFRest.Model;
+﻿using GraphQL;
+using GraphQL.Types;
+using ServicioWCFRest.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace ServicioWCFRest
 {
 
-    public class ServicioEmpleado : IServicioEmpleado
+    public class ServicioEmpleado : IServicioEmpleado , IGraphQLService
     {
+        private readonly ISchema _schema;
+        private readonly IDocumentExecuter _documentExecuter;
+
+        public ServicioEmpleado(ISchema schema)
+        {
+            _schema = schema;
+            _documentExecuter = new DocumentExecuter();
+        }
+
         public bool create(Empleado empleado)
         {
             using (NeptunoEntities db = new NeptunoEntities())
@@ -22,7 +37,7 @@ namespace ServicioWCFRest
                     emp.Nombre = empleado.Nombre;
                     emp.Cargo = empleado.Cargo;
                     emp.Dirección = empleado.Direccion;
-                    db.EmpleadoEntity.Add(emp);
+                    db.Empleados.Add(emp);
                     db.SaveChanges();
                     return true;
                 }
@@ -31,7 +46,7 @@ namespace ServicioWCFRest
                     return false;
                 }
             }
-        }//
+        }
 
         public bool delete(Empleado empleado)
         {
@@ -40,8 +55,8 @@ namespace ServicioWCFRest
                 try
                 {
                     int cod = Convert.ToInt32(empleado.IdEmpleado);
-                    EmpleadoEntity emp = db.EmpleadoEntity.Single(em => em.IdEmpleado == cod);
-                    db.EmpleadoEntity.Remove(emp);
+                    EmpleadoEntity emp = db.Empleados.Single(em => em.IdEmpleado == cod);
+                    db.Empleados.Remove(emp);
                     db.SaveChanges();
                     return true;
                 }
@@ -50,7 +65,7 @@ namespace ServicioWCFRest
                     return false;
                 }
             }
-        }//
+        }
 
         public bool edit(Empleado empleado)
         {
@@ -59,7 +74,7 @@ namespace ServicioWCFRest
                 try
                 {
                     int cod = Convert.ToInt32(empleado.IdEmpleado);
-                    EmpleadoEntity emp = db.EmpleadoEntity.Single(em => em.IdEmpleado == cod);
+                    EmpleadoEntity emp = db.Empleados.Single(em => em.IdEmpleado == cod);
                     emp.Apellidos = empleado.Apellidos;
                     emp.Nombre = empleado.Nombre;
                     emp.Cargo = empleado.Cargo;
@@ -72,14 +87,14 @@ namespace ServicioWCFRest
                     return false;
                 }
             }
-        }//
+        }
 
         public Empleado find(string id)
         {
             using (NeptunoEntities db = new NeptunoEntities())
             {
                 int cod = Convert.ToInt32(id);
-                return db.EmpleadoEntity.Where(em => em.IdEmpleado == cod).Select(em => new Empleado
+                return db.Empleados.Where(em => em.IdEmpleado == cod).Select(em => new Empleado
                 {
                     IdEmpleado = em.IdEmpleado,
                     Apellidos = em.Apellidos,
@@ -88,13 +103,13 @@ namespace ServicioWCFRest
                     Direccion = em.Dirección
                 }).First();
             }
-        }//
+        }
 
         public List<Empleado> readAll()
         {
             using (NeptunoEntities db = new NeptunoEntities())
             {
-                return db.EmpleadoEntity.Select(em => new Empleado
+                return db.Empleados.Select(em => new Empleado
                 {
                     IdEmpleado = em.IdEmpleado,
                     Apellidos = em.Apellidos,
@@ -103,7 +118,10 @@ namespace ServicioWCFRest
                     Direccion = em.Dirección
                 }).ToList();
             }
-        }//
-
+        }
+        public Message ExecuteGraphQL(Message request)
+        {
+            return null;
+        }
     }
 }
